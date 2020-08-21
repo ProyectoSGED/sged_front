e7697c8f9a5284d9bb96728812bb2427f18134f1
Alonso Bravo <abravo@movilform.com>
2020-08-21 13:05:12 -0400

    Se añaden variables de entorno para configuraciones de servidor

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
@@ -5,7 +6,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
