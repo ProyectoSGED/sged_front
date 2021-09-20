72fcd51f4323240283499f04780a2d02e0cb6d04
Alonso Bravo <abravo@movilform.com>
2021-09-20 18:53:40 -0300

    correccion

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 9000;
